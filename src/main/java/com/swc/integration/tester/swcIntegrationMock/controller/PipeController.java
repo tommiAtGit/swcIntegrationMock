034f42db5f158268e788e0e5dbabbde706a26937
Tommi <tommi.korteikko@iCloud.com>
2020-11-29 13:34:28 +0200

    Added functionality to handel pipe indormation

---
+++ src/main/java/com/swc/integration/tester/swcIntegrationMock/controller/PipeController.java
@@ -0,0 +1,44 @@
+package com.swc.integration.tester.swcIntegrationMock.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.swc.integration.tester.swcIntegrationMock.model.Pipe;
+import com.swc.integration.tester.swcIntegrationMock.model.web.PipeDto;
+import com.swc.integration.tester.swcIntegrationMock.service.PipeService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RequiredArgsConstructor
+@RequestMapping("/networks/{networkID}/pipes")
+@RestController
+public class PipeController {
+	
+	@Autowired
+	PipeService service;
+	
+	@PostMapping
+	public ResponseEntity<Pipe>savePipes( @Validated @PathVariable("networkID") String networkId, @Validated @RequestBody List<PipeDto> pioeDtos){
+		
+		if (networkId == null) {
+			log.error("..at savePioes, network id were null");
+		}
+		else {
+			log.info("..at saveSensors, network id OK! " + networkId );
+		}
+		
+		log.info(".. New sensors added. Number of sensors created: " + pioeDtos.size());
+		return new ResponseEntity<Pipe>(service.savePipe(pioeDtos),HttpStatus.CREATED);
+	
+	}
+}
---
+++ src/main/java/com/swc/integration/tester/swcIntegrationMock/service/PipeService.java
@@ -0,0 +1,11 @@
+package com.swc.integration.tester.swcIntegrationMock.service;
+
+import java.util.List;
+
+import com.swc.integration.tester.swcIntegrationMock.model.Pipe;
+import com.swc.integration.tester.swcIntegrationMock.model.web.PipeDto;
+
+public interface PipeService {
+
+	public Pipe savePipe(List<PipeDto> pipeDto);
+}
